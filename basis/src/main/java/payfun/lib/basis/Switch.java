7fcf00a8f4197305455b4a9b462c9d9c458da734
zhangqg <854627901@qq.com>
2023-04-28 17:30:46 +0800

    新增唤醒库key

--- basis/src/main/java/payfun/lib/basis/Switch.java
+++ basis/src/main/java/payfun/lib/basis/Switch.java
@@ -8,7 +8,7 @@ package payfun.lib.basis;
 public class Switch {
     public static final boolean lOG_SWITCH = true;
     public static final boolean VAD_AIUI = false;//vad渠道 true:AIUI  false:自研
-    public static final int VAD_AIUI_MODEL = 2;//自研VAD模型 1旧  2新模型
+    public static final int VAD_AIUI_MODEL = 2;//自研VAD模型 1旧  2新模型 只在VAD_AIUI=false下生效
 
 
 }
